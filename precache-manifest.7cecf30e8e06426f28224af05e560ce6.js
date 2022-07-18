0682f09db5cd8b064433a1eecffa71407164781f
Arz-zun <arjunchaudhary757@gmail.com>
2022-07-18 17:41:53 +0545

    Updates

---
+++ precache-manifest.7cecf30e8e06426f28224af05e560ce6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c415bb689a68068cb69",
+    "url": "/portfolio/static/css/main.2e044e2a.chunk.css"
+  },
+  {
+    "revision": "4c415bb689a68068cb69",
+    "url": "/portfolio/static/js/main.4c415bb6.chunk.js"
+  },
+  {
+    "revision": "cf7b992735a78877cbdb",
+    "url": "/portfolio/static/js/1.cf7b9927.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5569804e8e2a57bd9d45effd2c60c487",
+    "url": "/portfolio/static/media/image1.5569804e.jpg"
+  },
+  {
+    "revision": "ef41f3ae9a3eb01134135db0f030ec6e",
+    "url": "/portfolio/static/media/arjun.ef41f3ae.jpg"
+  },
+  {
+    "revision": "0c58af40694bc7b4626d9ecd21b0902f",
+    "url": "/portfolio/static/media/CV.0c58af40.pdf"
+  },
+  {
+    "revision": "9e4b812f3afcf3911f4cc9cc7c420cd9",
+    "url": "/portfolio/static/media/arjun1.9e4b812f.jpg"
+  },
+  {
+    "revision": "3c4dde2f339102d39c38869b4f273d0f",
+    "url": "/portfolio/static/media/bg.3c4dde2f.png"
+  },
+  {
+    "revision": "e0eee1ff829c3cbae0f807a23431a870",
+    "url": "/portfolio/index.html"
+  }
+];
